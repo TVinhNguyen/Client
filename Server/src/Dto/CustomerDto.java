abe8444b3bdf32b0234c92ee60e75df52c5ab811
TVinhNguyen <91182089+TVinhNguyen@users.noreply.github.com>
2024-11-20 20:08:49 +0700

    Update CustomerDto.java

--- Server/src/Dto/CustomerDto.java
+++ Server/src/Dto/CustomerDto.java
@@ -114,5 +114,76 @@ public class CustomerDto {
 		return null;
 	}
 	//cập nhập số tiền nạp 
-	
+	public static Customer getByLogin(String username, String password) throws SQLException {
+        String query = "SELECT * FROM Customer WHERE nameAccount = ? AND passwordAccount = ?";
+
+        try (Connection connection = DBConnection.getConnection();
+             PreparedStatement statement = connection.prepareStatement(query)) {
+            statement.setString(1, username);
+            statement.setString(2, password); 
+            ResultSet resultSet = statement.executeQuery();
+
+            if (resultSet.next()) {
+            	int idCustomer=resultSet.getInt("idCustomer");
+		    	String nameCustomer=resultSet.getString("nameCustomer");
+		    	String phoneCustomer=resultSet.getString("phoneCustomer");
+		    	String nameAccount=resultSet.getString("nameAccount");
+		    	String passwordAccount=resultSet.getString("passwordAccount");
+		    	int pointAccount=resultSet.getInt("pointAccount");
+		    	Long remainTime=resultSet.getLong("remainTime");
+		    	Double remainMoney=resultSet.getDouble("remainMoney");
+
+                return new Customer(idCustomer, nameCustomer, phoneCustomer, nameAccount, passwordAccount, pointAccount,remainTime,remainMoney );
+                
+            }
+        }
+
+        return null; 
+    }
+	public static void updateTime(int id , long time) throws SQLException {
+		String query =  "UPDATE customer SET remainTime = ? WHERE idCustomer = ?";
+		  try(Connection conn=DBConnection.getConnection();
+			PreparedStatement statement=conn.prepareStatement(query)) {
+			  statement.setLong(1, time);
+			  statement.setInt(2, id);
+			  int result=statement.executeUpdate();
+		    	if(result>0)
+		    	{
+		    		System.out.println("Cập nhật thời gian thành công");
+		    	}
+		    	else
+		    	{
+		    		System.out.println("Cập nhật thời gian thất bại ");
+		    	}
+		  }
+	}
+	public static void updateBalance(int id, double balance) throws SQLException {
+		String query =  "UPDATE customer SET remainMoney = ? WHERE idCustomer = ?";
+		  try(Connection conn=DBConnection.getConnection();
+			PreparedStatement statement=conn.prepareStatement(query)) {
+			  statement.setDouble(1, balance);
+			  statement.setInt(2, id);
+
+			  int result=statement.executeUpdate();
+		    	if(result>0)
+		    	{
+		    		System.out.println("Cập nhật tiền tài khoản");
+		    	}
+		    	else
+		    	{
+		    		System.out.println("Cập nhật tiền tài khoản thất bại ");
+		    	}
+		  }
+	}
+	 public static void depositToUser(int accountId, double amount) throws SQLException {
+	        String query = "UPDATE customer SET remainMoney = remainMoney + ? WHERE idCustomer = ?";
+	        
+	        try (Connection connection = DBConnection.getConnection();
+	             PreparedStatement statement = connection.prepareStatement(query)) {
+	            statement.setDouble(1, amount);
+	            statement.setInt(2, accountId);
+	            
+	            statement.executeUpdate();
+	        }
+	    }
 }
